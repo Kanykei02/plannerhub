b306448e10ac3ceae1f84b2ed9d0584cd408ee5e
Kanykei02 <almazbekovakany@gmail.com>
2021-05-01 18:07:40 +0600

    Initial commit general project

---
+++ src/main/java/kg/ItAcademy/plannerhub/PlannerhubApplication.java
@@ -0,0 +1,13 @@
+package kg.ItAcademy.plannerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/ItAcademy/plannerhub/PlannerhubApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.ItAcademy.plannerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
