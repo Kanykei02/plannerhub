a15dbce1997a954fbd6bd74d9ff92e1fa3e089bc
Kanykei02 <almazbekovakany@gmail.com>
2021-05-02 15:40:05 +0600

    push

--- src/main/java/kg/ItAcademy/plannerhub/service/UserServiceImpl.java
+++ src/main/java/kg/ItAcademy/plannerhub/service/UserServiceImpl.java
@@ -3,9 +3,11 @@ package kg.ItAcademy.plannerhub.service;
 import kg.ItAcademy.plannerhub.entity.User;
 import kg.ItAcademy.plannerhub.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
